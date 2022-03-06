bcc07f9e69f4798d26a9bad5c479bd900a9f24ef
Niatomi <playervoker@gmail.com>
2022-03-06 21:32:05 +0400

    Renamed to task7

--- task7/LCD.ino
+++ task7/LCD.ino
@@ -1,86 +1,174 @@
-#include <LiquidCrystal_I2C.h>
+#include <LCD_1602_RUS.h>
 
+#define RED 4
+#define GREEN 2
+#define YELLOW 3
+#define BUTTON A0
 #define LED_BRIGHT_PIN A0
-#define POTENTIOMETR_PIN A3
-#define BUTTON_PIN A4
 
-const int count = 100;
+byte blinkAmount = 2;
+unsigned int blinkTime = 1000;
+unsigned int timeWait = 10000;
+unsigned int timeWaitYellow = 3000;
+unsigned int remainingTime = 15000;
+unsigned long globalTimeBuffer = millis();
+unsigned long localeTimeBuffer = millis();
 
-LiquidCrystal_I2C LCD (0x27, 20, 4);
+String goPermission = "Можно ехать";
+String ready = "Приготовиться";
+String remainingTimeString = "Осталось времени:";
+String stopMessage = "Стоп!";
 
-byte squareSymbol[8] =
-    {
-        B11111,
-        B11111,
-        B11111,
-        B11111,
-        B11111,
-        B11111,
-        B11111,
-        B00000,
-};
+LCD_1602_RUS LCD (0x27, 20, 4);
 
 void setup() {
 
-    Serial.begin(9600);
-    
-    LCD.init();
-    LCD.backlight();
-    
-    pinMode(LED_BRIGHT_PIN, OUTPUT);
-    analogWrite(LED_BRIGHT_PIN, 250);
+  Serial.begin(9600);
 
-    pinMode(A3, INPUT_PULLUP);
-    pinMode(A4, INPUT_PULLUP);
+  pinMode(BUTTON, INPUT);
 
-    LCD.createChar(0, squareSymbol);
+  pinMode(GREEN, OUTPUT);
+  pinMode(YELLOW, OUTPUT);
+  pinMode(RED, OUTPUT);
 
-    LCD.clear();
+  digitalWrite(RED, 0);
+  digitalWrite(GREEN, 0);
+  digitalWrite(YELLOW, 0);
+
+  LCD.init();
+  LCD.backlight();
+  
+  pinMode(LED_BRIGHT_PIN, OUTPUT);
+  analogWrite(LED_BRIGHT_PIN, 250);
+
+  pinMode(A3, INPUT_PULLUP);
+  pinMode(A4, INPUT_PULLUP);
+
+  LCD.clear();
 
 }
 
-double buffer = (double) map(analogRead(POTENTIOMETR_PIN), 0, 1023, 0, 500)/100; 
+
+void writeTimeOnSecondAndThirdRows(String message, int time) {
+  int buffer = time/1000;
+  writeLCD(message);
+  LCD.setCursor(0, 1);
+  LCD.print(remainingTimeString);
+  LCD.setCursor(0, 2);
+  LCD.print(buffer);
+  LCD.print(" с.");
+  // LCD.println();
+}
+
+void writeLCD(String string) {
+  LCD.clear();
+  LCD.setCursor(0, 0);
+  LCD.print((string));
+}
+
+void greenBlinking() {
+  globalTimeBuffer = millis();
+  int blinkCounter = 0;
+  writeLCD(goPermission);
+  boolean exiteState_1 = true;
+  boolean exiteState_2 = true;
+  while (exiteState_1) {
+    digitalWrite(GREEN, 1);
+    globalTimeBuffer = millis();  
+    while (exiteState_2) {
+      if (millis() - globalTimeBuffer > blinkTime) {
+        exiteState_2 = false;
+        globalTimeBuffer = millis();
+      } 
+    }
+    exiteState_2 = true;
+
+    digitalWrite(GREEN, 0);
+    blinkCounter++;
+    globalTimeBuffer = millis();
+    while (exiteState_2) {
+      if (millis() - globalTimeBuffer > blinkTime) {
+        exiteState_2 = false;
+        globalTimeBuffer = millis();
+      } 
+    }
+    exiteState_2 = true;
+
+    if (blinkCounter == blinkAmount) exiteState_1 = false;
+  }
+}
+
+void greenToRed() {
+  boolean exitState = true;
+  byte redCounter = 0;
+  digitalWrite(GREEN, 0);
+  writeLCD(stopMessage);
+  digitalWrite(RED, 1);
+  globalTimeBuffer = millis();
+  localeTimeBuffer = millis();
+  while (exitState) {
+    if (millis() - localeTimeBuffer > 1000) {
+      writeTimeOnSecondAndThirdRows(stopMessage, timeWait - 1000*redCounter);
+      localeTimeBuffer = millis();
+      redCounter++;
+    } 
+    if (millis() - globalTimeBuffer > timeWait+2000) {
+      exitState = false;
+      localeTimeBuffer = millis();
+    } 
+  }
+
+}
+
+void waitYellow() {
+  boolean exitState = true;
+  digitalWrite(RED, 0);
+  digitalWrite(YELLOW, 1);
+  writeLCD(ready);
+  globalTimeBuffer = millis();
+  while (exitState) {
+    if (millis() - globalTimeBuffer > timeWaitYellow) {
+      exitState = false;
+    } 
+  }
+  digitalWrite(YELLOW, 0);
+}
+
+void greenLightTime() {
+  boolean exitState = true;
+  digitalWrite(GREEN, 1);
+  writeLCD(goPermission);
+  globalTimeBuffer = millis();
+  byte greenCounter = 0;
+  localeTimeBuffer = millis();
+  globalTimeBuffer = millis();
+  while (exitState) {
+    if (millis() - localeTimeBuffer > 1000) {
+      writeTimeOnSecondAndThirdRows(goPermission, remainingTime - 1000*greenCounter);
+      localeTimeBuffer = millis();
+      greenCounter++;
+    } 
+    if (millis() - globalTimeBuffer > remainingTime+2800) {
+      exitState = false;
+      localeTimeBuffer = millis();
+    } 
+  }
+}
 
 void loop() {
+
+  boolean button = true;
+
+  if (button) {
+
+    greenLightTime();
     
-    unsigned int potentiometr = analogRead(POTENTIOMETR_PIN);
-    boolean button = digitalRead(BUTTON_PIN);
-    double voltage = (double) map(potentiometr, 150, 1023, 0, 500)/100;
+    greenBlinking();
 
-    Serial.println(round(map(round(voltage), 0, 5, 0, 2000)/100)); 
-    // Serial.print(" ");
-    // Serial.println(voltage);
-    // Serial.println();
-    
+    greenToRed();
 
-    if (voltage != buffer) {
+    waitYellow();
 
-      LCD.clear();
+  }
 
-      LCD.setCursor(0, 0);
-      LCD.print("Potentiometr: ");
-      LCD.print(voltage);
-      LCD.print("V");
-
-      
-      Serial.println(buffer);
-
-      for (int i = 0; i < map(round(voltage), 0, 5, 0, 2000)/100; i++) {
-        
-        LCD.setCursor(i, 1);
-        LCD.write(byte(0));
-
-        LCD.setCursor(i, 2);
-        LCD.write(byte(0));
-
-        LCD.setCursor(i, 3);
-        LCD.write(byte(0));
-      } 
-
-      buffer = voltage;
-
-    }
-    
-  
-    delay(50);
 }
