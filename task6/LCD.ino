566c389ab7c18208ded1046aef628071d296437f
Niatomi <playervoker@gmail.com>
2022-03-06 21:33:02 +0400

    Task and code same

--- task6/LCD.ino
+++ task6/LCD.ino
@@ -8,6 +8,18 @@ const int count = 100;
 
 LiquidCrystal_I2C LCD (0x27, 20, 4);
 
+byte squareSymbol[8] =
+    {
+        B11111,
+        B11111,
+        B11111,
+        B11111,
+        B11111,
+        B11111,
+        B11111,
+        B00000,
+};
+
 void setup() {
 
     Serial.begin(9600);
@@ -21,28 +33,54 @@ void setup() {
     pinMode(A3, INPUT_PULLUP);
     pinMode(A4, INPUT_PULLUP);
 
+    LCD.createChar(0, squareSymbol);
+
     LCD.clear();
-    
+
 }
+
+double buffer = (double) map(analogRead(POTENTIOMETR_PIN), 0, 1023, 0, 500)/100; 
 
 void loop() {
     
     unsigned int potentiometr = analogRead(POTENTIOMETR_PIN);
     boolean button = digitalRead(BUTTON_PIN);
-    double buffer = (double) map(potentiometr, 0, 1023, 0, 500)/100; 
-  
-    LCD.clear();
+    double voltage = (double) map(potentiometr, 150, 1023, 0, 500)/100;
 
-    LCD.setCursor(0, 0);
-    LCD.print("Potentiometr: ");
-    LCD.print(buffer);
-    LCD.print("V");
-
-    LCD.setCursor(0, 1);
-    LCD.print("Button: ");
-    if (button) {
-      LCD.print("pressed");
-    } else LCD.print("not pressed");
+    Serial.println(round(map(round(voltage), 0, 5, 0, 2000)/100)); 
+    // Serial.print(" ");
+    // Serial.println(voltage);
+    // Serial.println();
     
-    delay(100);
+
+    if (voltage != buffer) {
+
+      LCD.clear();
+
+      LCD.setCursor(0, 0);
+      LCD.print("Potentiometr: ");
+      LCD.print(voltage);
+      LCD.print("V");
+
+      
+      Serial.println(buffer);
+
+      for (int i = 0; i < map(round(voltage), 0, 5, 0, 2000)/100; i++) {
+        
+        LCD.setCursor(i, 1);
+        LCD.write(byte(0));
+
+        LCD.setCursor(i, 2);
+        LCD.write(byte(0));
+
+        LCD.setCursor(i, 3);
+        LCD.write(byte(0));
+      } 
+
+      buffer = voltage;
+
+    }
+    
+  
+    delay(50);
 }
